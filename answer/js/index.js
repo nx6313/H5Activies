b7600a6d9ba9fe9eeb5a88e591d9313fc8cb1701
ngdong <2559635030@qq.com>
2018-03-30 10:30:52 +0800

    sdf

--- answer/js/index.js
+++ answer/js/index.js
@@ -2,6 +2,8 @@ var gameAudio = null;
 var gameAudioVolume = 0.6;
 
 var hasRotateClass = false;
+var xmlhttp = false;
+var gameUserData = {};
 // 开启或关闭游戏背景音效
 function playMusicToggle() {
     var musicDom = document.getElementById('musicPlayWrap');
@@ -29,6 +31,22 @@ function playMusicToggle() {
 
 // 开始游戏
 function startGame() {
+    var userName = document.getElementById('nameInput').value;
+    var userPhone = document.getElementById('phoneInput').value;
+    if (!userName) {
+        alert('请输入您的姓名');
+        return false;
+    }
+    if (!userPhone) {
+        alert('请输入您的手机号');
+        return false;
+    }
+    if (!/^1[3|4|5|6|7|8][0-9]\d{8}$/.test(userPhone.trim())) {
+        alert('不是有效的手机号');
+        return false;
+    }
+    gameUserData['username'] = userName.trim();
+    gameUserData['user_phone'] = userPhone.trim();
     var startGamePage = document.getElementById('gameStartPage');
     startGamePage.style.transform = 'translateY(-100vh)';
 
@@ -71,7 +89,6 @@ function showAnswerPage(pageIndex) {
             questionPageWrapDom.style.left = 'calc(' + (pageIndex - 1) + ' * 100vw)';
             questionPageWrapDom.style.width = '100vw';
             questionPageWrapDom.style.height = '100vh';
-            questionPageWrapDom.innerHTML = '我是第 ' + pageIndex + ' 页';
             gameTraiWrapDom.appendChild(questionPageWrapDom);
 
             var questionTitleWrapDom = document.createElement('div');
@@ -94,6 +111,41 @@ function showAnswerPage(pageIndex) {
                     answerItemDom.classList.add('answerItem');
                     answerItemDom.innerHTML = gameDataForThisPage['answers'][q]['key'] + '： ' + gameDataForThisPage['answers'][q]['val'];
                     questionPageWrapDom.appendChild(answerItemDom);
+                    answerItemDom.onclick = function () {
+                        var curSelectedItemDom = document.getElementById('question_' + pageIndex).getElementsByClassName('answerItemSelected')[0];
+                        if (curSelectedItemDom) {
+                            curSelectedItemDom.classList.remove('answerItemSelected');
+                        }
+                        this.classList.add('answerItemSelected');
+                        gameUserData['c_' + pageIndex] = this.innerHTML;
+                        if (pageIndex == questionData.length) {
+                            setTimeout(function () {
+                                var endGamePage = document.getElementById('gameEndPage');
+                                endGamePage.style.transform = 'translateY(0vh)';
+
+                                $.ajax({
+                                    type: 'post',
+                                    url: 'http://l.dachangjr.com/index.php?m=content&c=index&a=addchoujiang',
+                                    data: gameUserData,
+                                    success: function (msg) {
+                                        if (msg == '1') {
+                                            alert('提交成功');
+                                        } else {
+                                            alert('提交失败，请稍后重试');
+                                        }　　
+                                    }
+                                });
+                            }, 200);
+                        } else {
+                            setTimeout(function () {
+                                showAnswerPage(pageIndex + 1);
+
+                                gameAudio = new Audio('music/other.mp3');
+                                gameAudio.volume = gameAudioVolume;
+                                gameAudio.play();
+                            }, 200);
+                        }
+                    };
                 }
             } else if (gameDataForThisPage['type'] == 'input') {
                 var answerInputDom = document.createElement('textarea');
@@ -105,9 +157,25 @@ function showAnswerPage(pageIndex) {
             if (pageIndex == 1) {
                 var nextQuestionDom = document.createElement('span');
                 nextQuestionDom.classList.add('nextQuestion');
+                nextQuestionDom.classList.add('onlyOne');
                 nextQuestionDom.innerHTML = '下一题';
                 questionPageWrapDom.appendChild(nextQuestionDom);
                 nextQuestionDom.onclick = function () {
+                    if (gameDataForThisPage['type'] == 'input') {
+                        var answerInputDom = document.getElementById('question_' + pageIndex).getElementsByClassName('answerInput')[0];
+                        if (!answerInputDom.value) {
+                            alert('请填写您的答案');
+                            return false;
+                        }
+                        gameUserData['c_' + pageIndex] = answerInputDom.value;
+                    } else {
+                        var selectItemDom = document.getElementById('question_' + pageIndex).getElementsByClassName('answerItemSelected')[0];
+                        if (!selectItemDom) {
+                            alert('请选择您的答案');
+                            return false;
+                        }
+                        gameUserData['c_' + pageIndex] = selectItemDom.innerHTML;
+                    }
                     showAnswerPage(pageIndex + 1);
 
                     gameAudio = new Audio('music/other.mp3');
@@ -132,6 +200,21 @@ function showAnswerPage(pageIndex) {
                 nextQuestionDom.innerHTML = '下一题';
                 questionPageWrapDom.appendChild(nextQuestionDom);
                 nextQuestionDom.onclick = function () {
+                    if (gameDataForThisPage['type'] == 'input') {
+                        var answerInputDom = document.getElementById('question_' + pageIndex).getElementsByClassName('answerInput')[0];
+                        if (!answerInputDom.value) {
+                            alert('请填写您的答案');
+                            return false;
+                        }
+                        gameUserData['c_' + pageIndex] = answerInputDom.value;
+                    } else {
+                        var selectItemDom = document.getElementById('question_' + pageIndex).getElementsByClassName('answerItemSelected')[0];
+                        if (!selectItemDom) {
+                            alert('请选择您的答案');
+                            return false;
+                        }
+                        gameUserData['c_' + pageIndex] = selectItemDom.innerHTML;
+                    }
                     showAnswerPage(pageIndex + 1);
 
                     gameAudio = new Audio('music/other.mp3');
@@ -141,6 +224,7 @@ function showAnswerPage(pageIndex) {
             } else {
                 var preQuestionDom = document.createElement('span');
                 preQuestionDom.classList.add('preQuestion');
+                preQuestionDom.classList.add('onlyOne');
                 preQuestionDom.innerHTML = '上一题';
                 questionPageWrapDom.appendChild(preQuestionDom);
                 preQuestionDom.onclick = function () {
