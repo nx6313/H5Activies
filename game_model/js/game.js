ea213f5841d3ec6551ced106c2c42b5cb8b47a79
ngdong <2559635030@qq.com>
2018-04-17 16:29:07 +0800

    sdf

--- game_model/js/game.js
+++ game_model/js/game.js
@@ -4,7 +4,9 @@
 
     w.GamePluginOptions = {
         'orientation-vertical': 'vertical',
-        'orientation-horizontal': 'horizontal'
+        'orientation-horizontal': 'horizontal',
+        'playerController-left': 'left',
+        'playerController-right': 'right'
     };
 
     w.onresize = function () {
@@ -42,6 +44,24 @@
                 }
             }
         }
+        // var playerControllerWrap = d.getElementById('playerControllerWrap');
+        // if (playerControllerWrap) {
+        //     if (playerControllerWrap.classList.contains('playerController_' + GamePluginOptions['playerController-left'])) {
+        //         if (w.outerWidth < w.outerHeight) {
+        //             playerControllerWrap.style.top = '20px';
+        //         } else {
+        //             playerControllerWrap.style.bottom = '20px';
+        //         }
+        //         playerControllerWrap.style.left = '22px';
+        //     } else if (playerControllerWrap.classList.contains('playerController_' + GamePluginOptions['playerController-right'])) {
+        //         if (w.outerWidth < w.outerHeight) {
+        //             playerControllerWrap.style.left = '22px';
+        //         } else {
+        //             playerControllerWrap.style.right = '22px';
+        //         }
+        //         playerControllerWrap.style.bottom = '20px';
+        //     }
+        // }
     };
 
     var GamePlugin = function (options) {
@@ -50,6 +70,7 @@
         gamePlugin.bgColor = options.bgColor ? options.bgColor : null; // 页面背景图不足区域填充颜色
         gamePlugin.bgImg = options.bgImg ? options.bgImg : null; // 页面背景图片
         gamePlugin.fillColor = options.fillColor ? options.fillColor : 'transparent'; // canvas填充色
+        gamePlugin.playerControllerOffset = options.playerControllerOffset ? options.playerControllerOffset : GamePluginOptions['playerController-left']; // 角色控制器显示位置
         // 执行游戏区域canvas的初始化
         gamePlugin.gameCvs = (function initGameMap() {
             d.body.style.margin = '0';
@@ -130,9 +151,26 @@
         player: function (gamePlugin) {
             this.x = 0; // 角色的 x 坐标
             this.y = 0; // 角色的 y 坐标
-            this.update = function (x, y) {
-                this.x = x;
-                this.y = y;
+            this.isRuning = false; // 角色当前是否正在移动
+            this.moveToXStep = 0; // 角色当前朝X方向移动的步长
+            this.moveToYStep = 0; // 角色当前朝Y方向移动的步长
+            this.update = function (isRuning, x, y) {
+                this.isRuning = isRuning;
+                if (this.isRuning) {
+                    this.moveToXStep = x;
+                    this.moveToYStep = y;
+                } else {
+                    this.moveToXStep = 0;
+                    this.moveToYStep = 0;
+                }
+                this.x += this.moveToXStep;
+                this.y += this.moveToYStep;
+            };
+            this.keepRun = function () {
+                if (this.isRuning) {
+                    this.x += this.moveToXStep;
+                    this.y += this.moveToYStep;
+                }
             };
             this.render = function () {
                 gamePlugin.gameCvsCtx.save();
@@ -145,6 +183,7 @@
         startGame: function () {
             (function animate() {
                 gameMap.render();
+                gamePlayer.keepRun();
                 gamePlayer.render();
                 raf(animate);
             })();
@@ -153,11 +192,26 @@
         ///////////////////////////////////// 显示角色控制器
         showController: function () {
             var playerControllerWrapElem = d.createElement('div');
+            playerControllerWrapElem.id = 'playerControllerWrap';
             playerControllerWrapElem.style.position = 'absolute';
             playerControllerWrapElem.style.width = '18vw';
             playerControllerWrapElem.style.height = '18vw';
-            playerControllerWrapElem.style.left = '22px';
-            playerControllerWrapElem.style.bottom = '20px';
+            playerControllerWrapElem.classList.add('playerController_' + this.playerControllerOffset);
+            if (this.playerControllerOffset == GamePluginOptions['playerController-left']) {
+                if (w.outerWidth < w.outerHeight) {
+                    playerControllerWrapElem.style.top = '20px';
+                } else {
+                    playerControllerWrapElem.style.bottom = '20px';
+                }
+                playerControllerWrapElem.style.left = '22px';
+            } else if (this.playerControllerOffset == GamePluginOptions['playerController-right']) {
+                if (w.outerWidth < w.outerHeight) {
+                    playerControllerWrapElem.style.left = '22px';
+                } else {
+                    playerControllerWrapElem.style.right = '22px';
+                }
+                playerControllerWrapElem.style.bottom = '20px';
+            }
             playerControllerWrapElem.style.border = '1px dashed rgba(28, 173, 174, .6)';
             playerControllerWrapElem.style.borderRadius = '18vw';
             d.body.appendChild(playerControllerWrapElem);
@@ -174,37 +228,37 @@
             var oSX = playerControllerWrapElem.clientHeight / 2,
                 oSY = playerControllerWrapElem.clientWidth / 2;
             var oW, oH;
+            var moveControllerRate = 0.16;
+            var movePlayerRate = 0.1;
             playerControllerPointElem.addEventListener('touchstart', function (e) {
                 playerControllerPointElem.style.transition = 'none';
                 var touches = e.touches[0];
-                oW = touches.clientX - playerControllerPointElem.offsetLeft - playerControllerPointElem.clientWidth;
-                oH = touches.clientY - playerControllerPointElem.offsetTop - playerControllerPointElem.clientHeight;
+                oW = touches.clientX;
+                oH = touches.clientY;
                 playerControllerPointElem.addEventListener("touchmove", function () {
                     e.preventDefault();
                 }, false);
             }, false);
             playerControllerPointElem.addEventListener('touchmove', function (e) {
                 var touches = e.touches[0];
-                var oTop = (touches.clientY - oH) * 0.4;
-                var oLeft = (touches.clientX - oW) * 0.4;
+                var oLeft = (touches.clientX - oW) * moveControllerRate + oSX - playerControllerPointElem.clientWidth / 2;
+                var oTop = (touches.clientY - oH) * moveControllerRate + oSY - playerControllerPointElem.clientHeight / 2;
                 var currentX = playerControllerPointElem.offsetLeft + playerControllerPointElem.clientWidth / 2;
                 var currentY = playerControllerPointElem.offsetTop + playerControllerPointElem.clientHeight / 2;
-                var pointDistance = m.sqrt((currentX - oSX) * (currentX - oSX) + (currentY - oSY) * (currentY - oSY));
-                if (pointDistance <= playerControllerWrapElem.clientWidth / 2) {
-                    playerControllerPointElem.style.top = oTop + 'px';
-                    playerControllerPointElem.style.left = oLeft + 'px';
-                } else {
-                    playerControllerPointElem.style.top = oTop + 'px';
-                    playerControllerPointElem.style.left = oLeft + 'px';
-                }
+                var pointDistance = m.sqrt(m.pow((currentX - oSX), 2) + m.pow((currentY - oSY), 2));
+                playerControllerPointElem.style.top = oTop + 'px';
+                playerControllerPointElem.style.left = oLeft + 'px';
                 if (gamePlayer) {
-                    // gamePlayer.update();
+                    var moveToX = (currentX - oSX) * movePlayerRate;
+                    var moveToY = (currentY - oSY) * movePlayerRate;
+                    gamePlayer.update(true, moveToX, moveToY);
                 }
             }, false);
             playerControllerPointElem.addEventListener('touchend', function (e) {
                 playerControllerPointElem.style.transition = 'all 0.18s ease';
                 playerControllerPointElem.style.top = 'calc((18vw - 7vw) / 2)';
                 playerControllerPointElem.style.left = 'calc((18vw - 7vw) / 2)';
+                gamePlayer.update(false);
                 d.removeEventListener("touchmove", function () {
                     e.preventDefault();
                 }, false);
